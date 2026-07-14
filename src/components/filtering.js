fe577c7907f8e7bd2016db04fcef96857f17ac29
Iuliia Tsivileva <juliatsivileva@yandex.com>
2026-07-14 19:12:00 +0530

    1 project 6.2

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,34 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
+    Object.keys(indexes)                                    // Получаем ключи из объекта
+        .forEach((elementName) => {                         // Перебираем по именам
+            elements[elementName].append(                    // в каждый элемент добавляем опции
+                ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
+                .map(name => {  
+                    const el = document.createElement('option');
+                    el.textContent = name;
+                    el.value = name;
+                    return el;
+                })
+            );
+        });
 
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
+        if (action && action.getAttribute('name') === 'clear') {
+            const inputField = action.parentElement.querySelector('input');
+            if (inputField) {
+                inputField.value = '';
+            }
+            state[action.dataset.field] = '';
+        }
 
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
--- src/main.js
+++ src/main.js
@@ -5,9 +5,16 @@ import {data as sourceData} from "./data/dataset_1.js";
 
 import {initData} from "./data.js";
 import {processFormData} from "./lib/utils.js";
-
+// @todo: подключение
+import {initPagination} from "./components/pagination.js";
+// @todo: подключение
 import {initTable} from "./components/table.js";
 // @todo: подключение
+import {initSorting} from "./components/sorting.js";
+// @todo: подключение
+import {initFiltering} from "./components/filtering.js";
+// @todo: подключение
+import { initSearching } from "./components/searching.js"
 
 
 // Исходные данные используемые в render()
@@ -19,9 +26,13 @@ const {data, ...indexes} = initData(sourceData);
  */
 function collectState() {
     const state = processFormData(new FormData(sampleTable.container));
+    const rowsPerPage  = parseInt(state.rowsPerPage);
+    const page = parseInt(state.page ?? 1);
 
     return {
-        ...state
+        ...state,
+        rowsPerPage,
+        page
     };
 }
 
@@ -30,10 +41,17 @@ function collectState() {
  * @param {HTMLButtonElement?} action
  */
 function render(action) {
-    let state = collectState(); // состояние полей из таблицы
+    let state = collectState() ; // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
+    
     // @todo: использование
+    result = applySearching(result, state, action);
 
+    result = applyFiltering(result, state, action); // <-- ДОБАВИЛИ ФИЛЬТРАЦИЮ ПЕРЕД СОРТИРОВКОЙ
+
+    result = applySorting(result, state, action);
+
+    result = applyPagination(result, state, action);
 
     sampleTable.render(result)
 }
@@ -41,12 +59,33 @@ function render(action) {
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: [],
-    after: []
+    before: ['search', 'header', 'filter'],
+    after: ['pagination']
 }, render);
 
 // @todo: инициализация
+const applyPagination = initPagination(
+    sampleTable.pagination.elements,             // передаём сюда элементы пагинации, найденные в шаблоне
+    (el, page, isCurrent) => {                    // и колбэк, чтобы заполнять кнопки страниц данными
+        const input = el.querySelector('input');
+        const label = el.querySelector('span');
+        input.value = page;
+        input.checked = isCurrent;
+        label.textContent = page;
+        return el;
+    }
+);
 
+const applySorting = initSorting([
+    sampleTable.header.elements.sortByDate,
+    sampleTable.header.elements.sortByTotal
+]);
+
+const applyFiltering = initFiltering(sampleTable.filter.elements, {
+    searchBySeller: indexes.sellers
+});
+
+const applySearching = initSearching('search');
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
